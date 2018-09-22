a5a80d3f04d99fa1a506f8851a6a84354b587f81
marianamn <mariananaidenova@mail.bg>
2018-09-22 16:31:06 +0300

    setup: add linters and assets folder

--- public/src/components/App.tsx
+++ public/src/components/App.tsx
@@ -1,11 +1,60 @@
-import * as React from 'react';
+import * as React from "react";
+import styled, { injectGlobal } from "styled-components";
 
-interface IProps {
-    readonly name: string;
+// tslint:disable-next-line:no-unused-expression
+injectGlobal`
+  @font-face {
+    font-family: Roboto-Regular;
+    src: url("assets/fonts/Roboto-Regular.ttf") format("opentype");
+  }
+
+  @font-face {
+    font-family: RobotoCondensed-Regular;
+    src: url("assets/fonts/RobotoCondensed-Regular.ttf") format("opentype");
+  }
+
+  @font-face {
+    font-family: RobotoCondensed-Italic;
+    src: url("assets/fonts/RobotoCondensed-Italic.ttff") format("opentype");
+  }
+
+  @font-face {
+    font-family: RobotoCondensed-Bold;
+    src: url("assets/fonts/RobotoCondensed-Bold.ttf") format("opentype");
+  }
+
+  @font-face {
+    font-family: RobotoCondensed-Light;
+    src: url("assets/fonts/RobotoCondensed-Light.ttf") format("opentype");
+  }
+
+  body {
+    padding: 0;
+    margin: 0;
+    font-family: Roboto-Regular, sans-serif;
+  }
+`;
+
+export const Title = styled("h2")`
+  color: #003764;
+`;
+
+export const Logo = styled("img")`
+  width: 280px;
+  height: 180px;
+`;
+
+interface Props {
+  readonly name: string;
 }
 
-export default class App extends React.Component<IProps> {
-    render() {
-        return <h1>Hello {this.props.name}</h1>;
-    }
+export default class App extends React.Component<Props> {
+  render(): JSX.Element {
+    return (
+      <div>
+        <Logo src="assets/images/logo.jpg" />
+        <Title>Hello {this.props.name}</Title>
+      </div>
+    );
+  }
 }
--- public/src/index.tsx
+++ public/src/index.tsx
@@ -1,7 +1,7 @@
-import * as React from 'react';
-import * as ReactDom from 'react-dom';
-import App from './components/App';
+import * as React from "react";
+import * as ReactDom from "react-dom";
+import App from "./components/App";
 
-const root = document.querySelector('#container');
+const root = document.querySelector("#container");
 
-ReactDom.render(<App name='React'/>, root);
+ReactDom.render(<App name="React" />, root);
